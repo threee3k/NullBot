2bfa216b4c481c293ac9811ebe73c1cf62317e47
threee3k <152722128+threee3k@users.noreply.github.com>
2024-01-26 20:50:41 +0100

    Update chatbot.py

--- robot/chatbot.py
+++ robot/chatbot.py
@@ -13,14 +13,12 @@ intents = json.loads(open('intents.json').read())
 
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
-model = load_model('chatbotmodel.h5')
+model = load_model('chatbot_model.h5')
 
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
     return sentence_words
-
-
 
 def bag_of_words(sentence):
     sentence_words = clean_up_sentence(sentence)
@@ -28,29 +26,28 @@ def bag_of_words(sentence):
     for w in sentence_words:
         for i, word in enumerate(words):
             if word == w:
-                bag [i] == 1
+                bag[i] = 1  # Corrected from '==' to '='
     return np.array(bag)
-
 
 def predict_class(sentence):
     bow = bag_of_words(sentence)
     res = model.predict(np.array([bow]))[0]
     ERROR_THRESHOLD = 0.25
-    reuslt = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    result = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
 
     result.sort(key=lambda x: x[1], reverse=True)
     return_list = []
-    for r in reuslts:
+    for r in result:
         return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
     return return_list
 
-def get_response(intents_list, intents.json):
+def get_response(intents_list, intents_json):
     tag = intents_list[0]['intent']
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
         if i['tag'] == tag:
             result = random.choice(i['responses'])
-        break
+            break  # Corrected indentation
     return result
 
 print("Hello world")
